d12197f2651758be6c8f4e5325437b81c006ec81
kyryl-web <kkimarski@gmail.com>
2020-07-10 21:51:11 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,46 @@
-$(function(){
+$(function () {
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: '$'
+    });
 
-    var mixer = mixitup('.products__inner-box');
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
+    });
+
+    $('.icon-th-list').addClass('list');
 
     $('.rate-star').rateYo({
         rating: 5,
         starWidth: '12px',
         readOnly: true
     });
+
+
+    var mixer = mixitup('.products__inner-box');
+
+
     $('.product-slider__inner').slick({
         dots: true,
-        slidesToShow: 4,
+        slidesToShow: 3,
         arrows: false,
-        slidesToScroll: 4,
+        slidesToScroll: 3,
     });
 
- 
+
+
+
+
 });
