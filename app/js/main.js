da13bec28afb140cb4fdd591861836697560fc67
kyryl-web <kkimarski@gmail.com>
2020-07-14 12:04:53 +0300

    added icons in header, adaptive footer and header

--- app/js/main.js
+++ app/js/main.js
@@ -9,16 +9,17 @@ $(function () {
         prefix: '$'
     });
 
-    $('.icon-th-list').on('click', function(){
+    $('.icon-th-list').on('click', function () {
         $('.product__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
     });
-    $('.icon-th-large').on('click', function(){
+    $('.icon-th-large').on('click', function () {
         $('.product__item').removeClass('list');
         $('.icon-th-list').removeClass('active');
         $('.icon-th-large').addClass('active');
     });
+
 
     $('.icon-th-list').addClass('list');
 
@@ -28,10 +29,6 @@ $(function () {
         readOnly: true
     });
 
-
-    var mixer = mixitup('.products__inner-box');
-
-
     $('.product-slider__inner').slick({
         dots: true,
         slidesToShow: 3,
@@ -39,8 +36,16 @@ $(function () {
         slidesToScroll: 3,
     });
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+    
 
+    var mixer = mixitup('.products__inner-box');
 
 
 });
